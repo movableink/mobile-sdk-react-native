6177f3dadbe1ddd2b6ba3974c9c48d4385606e83
Chayel J Heinsen <cheinsen@movableink.com>
2023-05-09 14:16:37 -0400

    chore: initial commit

---
+++ android/src/main/java/com/movableink/MovableInkPackage.kt
@@ -0,0 +1,17 @@
+package com.movableink
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class MovableInkPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(MovableInkModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  MovableInkExample
+//
+
+import Foundation
