928371813f2af28701fad833498ae2237f53a37c
Chayel J Heinsen <cheinsen@movableink.com>
2023-05-22 13:44:57 -0400

    update android bundle id

--- example/android/app/src/main/java/com/movableink/MainActivity.java
+++ example/android/app/src/main/java/com/movableink/MainActivity.java
@@ -1,4 +1,4 @@
-package com.movableinkexample;
+package com.movableink;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- example/android/app/src/main/java/com/movableink/MainApplication.java
+++ example/android/app/src/main/java/com/movableink/MainApplication.java
@@ -1,4 +1,4 @@
-package com.movableinkexample;
+package com.movableink;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
